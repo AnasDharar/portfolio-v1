d5fc631f4b042ca1fb6da4297f11b374a9c8c70e
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-12-18 16:13:45 +0530

    under construction

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center border-2 bg-neutral-100 border-neutral-500 p-16 rounded-2xl shadow-md shadow-black'>
+        <h3>Portfolio under Construction</h3>
+        <p className='text-neutral-400 text-center'>
+          I am working to finish my portfolio. Please check back soon!
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App

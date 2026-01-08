9fe2718dd2244713bff746a5d01c925a59a9d97c
Anas <86509653+AnasDharar@users.noreply.github.com>
2026-01-08 11:11:08 +0530

    vercel analytics

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,5 +5,8 @@ import { Analytics } from "@vercel/analytics/react"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+    <>
     <App />
+    <Analytics />
+    </>
 )
